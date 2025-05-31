74fe75c19c70d4254902aeac7a4eb19f3d3b5854
protomem <protomem0709@gmail.com>
2025-05-31 23:11:42 +0300

    Move logger to stdx

--- crokd/src/main.rs
+++ crokd/src/main.rs
@@ -4,7 +4,7 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-use stdx::Error;
+use stdx::{Error, Logger};
 
 const DEFAULT_HTTP_ADDR: &str = "127.0.0.1:8080";
 
@@ -45,27 +45,6 @@ impl Config {
             Some(value) => value,
             None => default.to_string(),
         }
-    }
-}
-
-#[derive(Debug, Default, Clone)]
-struct Logger {
-    context_stack: Vec<String>,
-}
-
-impl Logger {
-    pub fn with(&self, context: &str) -> Self {
-        let mut new_logger = self.clone();
-        new_logger.context_stack.push(format!("[{}]", context));
-        new_logger
-    }
-
-    pub fn log(&self, message: &str) {
-        println!("{} {}", self.full_context(), message);
-    }
-
-    fn full_context(&self) -> String {
-        self.context_stack.join("")
     }
 }
 
